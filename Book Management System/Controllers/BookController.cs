a0712e6e69950e99cfd4cf97fe14843f6340e413
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-03-11 22:15:38 +0100

    Adding book api

---
+++ Book Management System/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Book_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var books = await _bookRepository.GetBooksAsync();
+
+            return Ok(books);
+            
+        }
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetById(int id)
+        {
+            var book = await _bookRepository.GetByIdAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();   
+            }
+
+            return Ok(book);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Book book)
+        {
+            await _bookRepository.AddAsync(book);
+
+            return CreatedAtAction(nameof(GetById), new { id = book.Id }, book);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update (int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            await _bookRepository.UpdateAsync(book);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _bookRepository.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
--- Book Management System/Program.cs
+++ Book Management System/Program.cs
@@ -1,4 +1,6 @@
-
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace Book_Management_System
@@ -9,29 +11,24 @@ namespace Book_Management_System
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
