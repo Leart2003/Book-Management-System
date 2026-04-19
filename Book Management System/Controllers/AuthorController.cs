e534d1439192445a529155c8d849670d003e5eca
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-04-20 00:46:53 +0200

    Adding author controller and category controller

---
+++ Book Management System/Controllers/AuthorController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var authors = _authorRepository.GetAllAsync();
+
+            return Ok(authors); 
+        }
+    }
+}
---
+++ Book Management System/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+
+            return Ok(categories);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult>GetById(int id)
+        {
+            var categories = await _categoryRepository.GetAsyncById(id);
+            if (categories is null)
+            {
+                return NotFound();
+            }
+            return Ok(categories);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _categoryRepository.AddAsync(category);
+
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id,Category category)
+
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+            await _categoryRepository.UpdateAsync(category);
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
