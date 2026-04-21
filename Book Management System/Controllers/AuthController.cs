d9acd57c32c3f24a5cbade324ec39d2327b5b5cb
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-04-21 23:37:26 +0200

    Adding authentication controller

---
+++ Book Management System/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Domain.DTOs;
+
+namespace Book_Management_System.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
