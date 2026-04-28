02528632809abcc5f5275f5c41514691fbed7d53
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-04-28 22:16:43 +0200

    adding changes

--- Book Management System/Controllers/AuthController.cs
+++ Book Management System/Controllers/AuthController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Domain.DTOs;
+using Domain.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Domain.Entities;
 
 namespace Book_Management_System.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration, UserManager<User> userManager)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
