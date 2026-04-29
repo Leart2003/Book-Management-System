11ca198be3406dd6f40c7309a2415527adda7ab3
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-04-29 19:33:27 +0200

    fixing error

--- Book Management System/Controllers/AuthController.cs
+++ Book Management System/Controllers/AuthController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Dtos;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Book_Management_System.Controllers
 {
@@ -7,5 +14,72 @@ namespace Book_Management_System.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration, UserManager<User> userManager)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+
+            var user = new User
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                UserName = registerDto.Email,
+                Email = registerDto.Email,
+
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("User register succesfully");
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null)
+                return Ok("User not found!");
+
+            var validPass = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            if (!validPass)
+                return Ok("Wrong password!");
+
+            var token = GenerateJwtToken(user);
+            return Ok(new { token });
+        }
+
+        private String GenerateJwtToken(User user)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var secret = jwtSettings["Secret"];
+
+            var claims = new[]
+          {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(7),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+
     }
 }
--- Infrastructure/DB/ApplicationDbContext.cs
+++ Infrastructure/DB/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 
 
+
 public class ApplicationDbContext : IdentityDbContext<User>
 {
     
@@ -19,6 +20,8 @@ public class ApplicationDbContext : IdentityDbContext<User>
     public DbSet<Book> Books => Set<Book>();
     public DbSet<Author> Authors => Set<Author>();
     public DbSet<Category> Categories => Set<Category>();
+    
+  
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
