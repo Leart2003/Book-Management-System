e9292c89b27ba4b47066812578395d9fd5a2ec47
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-05-03 21:41:52 +0200

    Adding new AddToFavorteAPI

---
+++ Domain/Entities/Koment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Koment
+    {
+        public int Id { get; set; }
+
+        public string? Content { get; set; }
+
+
+    }
+}
