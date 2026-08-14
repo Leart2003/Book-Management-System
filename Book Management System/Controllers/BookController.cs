8a2c8c18649abee7e06049c3900e5f5ce6b63d36
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-14 23:52:29 +0200

    documenting book controller

--- Book Management System/Controllers/BookController.cs
+++ Book Management System/Controllers/BookController.cs
@@ -17,6 +17,10 @@ namespace Book_Management_System.Controllers
             _bookService = bookService;
         }
 
+        /// <summary>
+        /// Get all books fron repository
+        /// </summary>
+        /// <returns>Return all existing books</returns>
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -25,6 +29,11 @@ namespace Book_Management_System.Controllers
             return Ok(books);
             
         }
+        /// <summary>
+        /// Gets a book by bookId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns the request according to bookId</returns>
         [HttpGet("{id}")]
 
         public async Task<IActionResult> GetById(int id)
@@ -39,12 +48,29 @@ namespace Book_Management_System.Controllers
             return Ok(book);
 
         }
+        /// <summary>
+        /// Create a book new book
+        /// </summary>
+        /// <param name="bookDto">The data required to create the book.</param>
+        /// <returns>Return the newly created book</returns>
+        /// /// <response code="201">The book was successfully created.</response>
+
         [HttpPost]
         public async Task<IActionResult> Create([FromForm]BookDto bookDto)
         {
             var book = await _bookService.CreateBook(bookDto);
             return CreatedAtAction(nameof(GetById), new { id = book.Id }, book);
         }
+
+        /// <summary>
+        /// Updates an existing book.
+        /// </summary>
+        /// <param name="id">The ID of the book to update</param>
+        /// <param name="book">The updated book information</param>
+        /// <returns>Return no content
+        /// </returns>
+        /// /// <response code="204">The book was successfully updated.</response>
+        /// <response code="400">The ID in the URL does not match the book ID.</response>
         [HttpPut("{id}")]
         public async Task<IActionResult> Update (int id, Book book)
         {
@@ -55,6 +81,14 @@ namespace Book_Management_System.Controllers
             await _bookService.UpdateBookAsync(book);
             return NoContent();
         }
+        /// <summary>
+        ///  Deletes an existing book.
+        /// </summary>
+        /// <param name="id">The ID of the book to delete.</param>
+        /// <returns>
+        /// Returns <see cref="NoContentResult"/> if the book was successfully deleted.
+        /// </returns>
+        /// <response code="204">The book was successfully deleted.</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
