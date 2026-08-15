2b02eb6b5e02d3bb07252831cc739043100e04f0
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-15 23:28:08 +0200

    documenting favorite controller

--- Book Management System/Controllers/FavoriteController.cs
+++ Book Management System/Controllers/FavoriteController.cs
@@ -16,7 +16,12 @@ namespace Book_Management_System.Controllers
         {
             _favoriteService = favoriteService;
         }
-
+        /// <summary>
+        /// Gets the favorite book of currently authenticated user
+        /// </summary>
+        /// <returns>Return the user's favorite book if authenticated.If not authenticated returns Unathorized</returns>
+        /// <response code="200">The user's favorite books were successfully retrieved.</response>
+        /// <response code="401">The user is not authenticated.</response>
         [HttpGet]
         public async Task<IActionResult> GetUserFavorite()
         {
@@ -30,6 +35,15 @@ namespace Book_Management_System.Controllers
             return Ok(favorite);
         }
 
+        /// <summary>
+        /// Add a book to user's favorte list.
+        /// </summary>
+        /// <param name="bookId">The id of the book to be added to favorite List</param>
+        /// <returns>Returns Book added to favorite list if the book was added.
+        /// If user is not authenticated return Unathorized
+        /// </returns>
+        /// /// <response code="200">The book was successfully added to the favorite list.</response>
+        /// <response code="401">The user is not authenticated.</response>
         [HttpPost]
         public async Task<IActionResult> AddToFavorite(int bookId)
         {
@@ -44,6 +58,13 @@ namespace Book_Management_System.Controllers
             return Ok("Book added to favorite List");
         }
 
+        /// <summary>
+        /// Removes a book from user's favorite list
+        /// </summary>
+        /// <param name="bookId">The id to be removed from user's favorite list</param>
+        /// <returns>
+        /// If removed returns a success message</returns>
+        /// <response code="200">Book removed succesfully from favorite the list</response>
         [HttpDelete]
 
         public async Task<IActionResult> RemoveFromFavorite(int bookId)
@@ -53,7 +74,15 @@ namespace Book_Management_System.Controllers
             return Ok("Book removed from favorite List");
         }
 
-
+        /// <summary>
+        /// Checks if book is already in user's favorite list
+        /// </summary>
+        /// <param name="bookId">The id to be checked if is in favorite list</param>
+        /// <returns>
+        /// Returns a boolean indicating whether the book is marked as a favorite.
+        ///
+        /// </returns>
+     
         [HttpGet("check/{bookId}")]
         public async Task<IActionResult> IsFavorite(int bookId)
         {
