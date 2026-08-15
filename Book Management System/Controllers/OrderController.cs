9be4d2403c5d97def5127caef5917c0af4638b74
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-15 23:33:07 +0200

    documenting Order controller

--- Book Management System/Controllers/OrderController.cs
+++ Book Management System/Controllers/OrderController.cs
@@ -18,6 +18,10 @@ namespace Book_Management_System.Controllers
         {
             _orderService = orderService;
         }
+        /// <summary>
+        /// Get all user's orders
+        /// </summary>
+        /// <returns>Returns all orders made by the user if authenticated, if not authenticated returns Unauthorized</returns>
 
         [HttpGet]
         public async Task<IActionResult> GetUserOrders() 
@@ -32,7 +36,11 @@ namespace Book_Management_System.Controllers
 
             return Ok (order);
         }
-
+        /// <summary>
+        /// Get an order by Id
+        /// </summary>
+        /// <param name="orderId">The id of order to be retrived</param>
+        /// <returns>returns the order</returns>
         [HttpGet("{orderId}")]
 
         public async Task<IActionResult> GetOrderID(int orderId)
@@ -41,6 +49,13 @@ namespace Book_Management_System.Controllers
 
             return Ok(order);
         }
+        /// <summary>
+        /// Creates a new order for the currently authenticated user.
+        /// </summary>
+        /// <param name="bookIds">A list of book IDs to include in the order.</param>
+        /// <returns>Returns the newly created order, or <see cref="UnauthorizedResult"/> if the user is not authenticated.</returns>
+        /// /// <response code="201">The order was successfully created.</response>
+        /// <response code="401">The user is not authenticated.</response>
         [HttpPost]
 
         public async Task<IActionResult> CreateOrder(List<int> bookIds)
@@ -53,6 +68,14 @@ namespace Book_Management_System.Controllers
             var order = await _orderService.CreateOrderAsync(userId, bookIds);
             return CreatedAtAction(nameof(GetOrderID), new { orderId = order.Id }, order);
         }
+        /// <summary>
+        /// Updates the status of the user's order
+        /// </summary>
+        /// <param name="orderId">The id of the order's status to be updated</param>
+        /// <param name="status"></param>
+        /// Returns <see cref="NoContentResult"/> if the order status was successfully updated.
+        /// </returns>
+        /// <response code="204">The order status was successfully updated.</response>
         [HttpPut("{orderId}/status")]
         public async Task<IActionResult> UpdateOrderStatus(int orderId, string status)
         {
