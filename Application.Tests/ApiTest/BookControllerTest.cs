7fb9d6880c09f6c48fa20f24e5b63df12ea9d41b
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-18 22:17:25 +0200

    testing enviroment

---
+++ Application.Tests/ApiTest/BookControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Tests.ApiTest
+{
+
+
+    public class BookControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
