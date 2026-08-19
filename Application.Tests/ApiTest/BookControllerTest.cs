89a3e17fd9d0f06e52ffaba925b6b8640c9afefa
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-19 23:49:44 +0200

    first testing

--- Application.Tests/ApiTest/BookControllerTest.cs
+++ Application.Tests/ApiTest/BookControllerTest.cs
@@ -12,8 +12,15 @@ namespace Application.Tests.ApiTest
     public class BookControllerTest
     {
         [Fact]
-        public async Task GetAllBooksTest()
+        public async Task Test1()
         {
+            var book = new Book { Title = "Clean Code", Price = 20 };
+
+            // Act
+            bool isValid = book.Price > 0;
+
+            // Assert
+            Assert.True(isValid);
         }
     }
 }
