b28386cec441e5a617f93d71a8618b4261c3ec75
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-22 22:55:05 +0200

    adding author test folder, and get all authors test

---
+++ Application.Tests/ApiTest/AuthorTest.cs
@@ -0,0 +1,32 @@
+﻿using Book_Management_System.Controllers;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Tests.ApiTest
+{
+    public class AuthorTest
+    {
+
+        public async Task GetAllAuthorTest()
+        {
+            var mockRepo = new Mock<IAuthorRepository>();
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(new List<Author>
+            {
+                new Author { Id = 1, FirstName = "George Orwell" },
+                new Author { Id = 2, FirstName = "Isaac Asimov" }
+            });
+            var controller = new AuthorController(mockRepo.Object);
+            var result = await controller.GetAll();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var authors = Assert.IsAssignableFrom<IEnumerable<Author>>(okResult.Value);
+            Assert.Equal(2, authors.Count());
+        }
+    }
+
+}
