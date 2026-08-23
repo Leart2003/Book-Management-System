ac0cc44187a180207020a7082b7aecf90616d13f
leartmaloku <113311081+Leart2003@users.noreply.github.com>
2026-08-23 23:40:21 +0200

    adding rating method test

---
+++ Application.Tests/RatingTesting.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Tests
+{
+    public class RatingTesting
+    {
+        [Fact]
+       
+        public async Task RateBookAsync_CallsRepositoryAddRatingAsync_WhenStarsAreValid()
+        {
+            var mockRepo = new Mock<IRatingInterface>();
+            var service = new RatingService(mockRepo.Object);
+
+            
+            await service.RateBookAsync(1, "user1", 4);
+
+         
+            mockRepo.Verify(r => r.AddRatingAsync(It.IsAny<Rating>()), Times.Once);
+        }
+
+    }
+}
